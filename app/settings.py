13ed8fc70f6a653eca2b8fbd0b9796b40631ee17
gregory <icedevil777111@mail.ru>
2023-01-23 10:11:49 +0300

    settings.py instead config.py

---
+++ app/settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: str
+    allowed_origins: set
+
+
+settings = Settings(
+    _env_file='../.env',
+    _env_file_encoding='utf-8'
+)
